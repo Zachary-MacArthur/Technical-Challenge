8506e4765c545ba3793de1748578b8d0b693b72c
Zachary MacArthur <91560582+Zachary-MacArthur@users.noreply.github.com>
2025-01-02 23:31:06 -0500

    reversed history

--- components/number-generator/NumberHistory.js
+++ components/number-generator/NumberHistory.js
@@ -3,11 +3,15 @@
 import React from 'react';
 
 const NumberHistory = ({ history, title }) => {
+  const sortedHistory = [...history].reverse();  
+
   return (
     <div className="mt-8">
-      <h3 className="text-lg cursor-default opacity-80 font-medium mb-3">{title}</h3>
+      {history.length > 0 && (
+        <h3 className="text-lg cursor-default opacity-80 font-medium mb-3">{title}</h3>
+      )}
       <div className="max-h-64 overflow-y-auto space-y-1">
-        {history.map((item, index) => (
+        {sortedHistory.map((item, index) => (
           <div 
             key={`${item.num1}-${item.num2}-${index}`}
             className="py-2 cursor-default border-b text-sm flex justify-between"
